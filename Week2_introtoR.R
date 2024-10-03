0357ced50ab85fe0fb615dcd91e6caa266e3edcb
Sam Stromberg <smstromberg@ucdavis.edu>
2024-10-03 13:56:10 -0700

    week 2 elephant script

---
+++ Week2_introtoR.R
@@ -0,0 +1,45 @@
+3 + 4
+1 +
+4 + 8*3^2
+4 + 8 * 3 ^ 2
+sqrt(25)
+
+exp(9)
+?log
+log(2,4)
+log(4,2)
+log(x = 2, base = 4)
+log(base = 4, x = 2)
+x = 1
+x <- 1
+x <-2
+x = 3
+x
+rm(x)
+sqrt(exp(4))
+log(exp(100))
+mynumber <-6
+mynumber==6
+mynumber == 7
+?morley
+mynumber != 0
+mynumber !!=0
+othernumber <- -3
+mynumber * othernumber
+rm(list = ls)
+rm (list = ls())
+ls
+mynumber <- 6
+ls
+rm
+log_of_word <- log("a_word)")
+log_of_negative <- log(-2)
+log_of_negative
+log_of_word
+log_of_negative
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+elephant1_lb <- elephant1_kg *2.2
+elephant2_lb > elephant1_lb
+myelephants <- (c(elephant1_lb, elephant2_lb))
+which(myelephants == max(myelephants))
