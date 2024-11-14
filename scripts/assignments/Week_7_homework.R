95323d21fdd06e162dfe342023a2c39ba34ad068
sammstromberg <sammstromberg@penguin>
2024-11-14 13:31:02 -0800

    coord_flip

--- scripts/assignments/Week_7_homework.R
+++ scripts/assignments/Week_7_homework.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 gapminder <- read_csv("https://ucd-r-davis.github.io/R-DAVIS/data/gapminder.csv")
 
-gapminder %>%filter(continent != "Oceania") %>%  group_by(country, continent) %>%  pivot_wider(id_cols = c(country, continent), names_from = year, values_from = pop) %>% select(continent, country, "2002", "2007") #creates tibble w continent, country, 2002 pop, and 2007 pop
+gapminder %>%filter(continent != "Oceania") %>%  group_by(country, continent) %>%  pivot_wider(id_cols = c(country, continent), names_from = year, values_from = pop) %>% select(continent, country, `2002`, `2007`) #creates tibble w continent, country, 2002 pop, and 2007 pop
 
 ggplot(data = gapminder %>% 
          filter(continent != "Oceania") %>% 
@@ -14,7 +14,8 @@ ggplot(data = gapminder %>%
   facet_wrap(~continent, scales = "free", axes = "all") + 
   theme_light() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   labs(x = "Country", y = "Change in Population Between 2002 and 2007") + 
-  scale_fill_viridis_d(option = "D")
+  scale_fill_viridis_d(option = "D") +
+  coord_flip()
 
 
 
